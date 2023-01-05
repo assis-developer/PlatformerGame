69baa553d1b3d84c63dc03e25dbe83759e3d56e7
Lucas Assis Borges <z216170@corp.clarobr>
2023-01-04 23:28:45 -0300

    Criação da aplicação base

---
+++ src/main/GameWindow.java
@@ -0,0 +1,19 @@
+package main;
+
+import javax.swing.*;
+
+public class GameWindow extends JFrame {
+
+    private JFrame jFrame;
+
+    public GameWindow(GamePanel gamePanel){
+        jFrame = new JFrame();
+
+        jFrame.setSize(400,400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(gamePanel);
+        jFrame.setVisible(true);
+
+    }
+
+}
