2d2714e98e3d86b92cec69832603f850eeb91d0d
Lucas Assis Borges <z216170@corp.clarobr>
2023-01-06 03:20:14 -0300

    Adicionando Collision Detection

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,50 +1,51 @@
 package entities;
 
+import main.Game;
 import utils.LoadSave;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
-import static utils.Constants.Directions.*;
-import static utils.Constants.Directions.DOWN;
 import static utils.Constants.PlayerConstant.*;
+import static utils.HelpMethods.CanMoveHere;
 
-public class Player extends Entity{
+public class Player extends Entity {
 
     private BufferedImage[][] animations;
-    private int aniTick, aniIndex, aniSpeed = 15;
+    private int aniTick, aniIndex, aniSpeed = 25;
 
     private int playerAction = IDLE;
     private boolean moving = false, attacking = false;
     private boolean left, up, right, down;
     private float playerSpeed = 2.0f;
+    private int[][] lvlData;
+    private float xDrawOffset = 21 * Game.SCALE;
+    private float yDrawOffset = 4 * Game.SCALE;
 
 
-    public Player(float x, float y) {
-        super(x, y);
+    public Player(float x, float y, int width, int height) {
+        super(x, y, width, height);
         loadAnimations();
+        initHitBox(x, y, 20 * Game.SCALE, 28 * Game.SCALE);
     }
 
-    public void update(){
+    public void update() {
+        updatePos();
         updateAnimationTick();
         setAnimation();
-        updatePos();
     }
 
-    public void render(Graphics g){
-        g.drawImage(animations[playerAction][aniIndex], (int) x, (int) y, 128, 80, null);
+    public void render(Graphics g) {
+        g.drawImage(animations[playerAction][aniIndex], (int) (hitBox.x - xDrawOffset), (int) (hitBox.y - yDrawOffset), 128, 80, null);
+        drawHitBox(g);
     }
 
     private void updateAnimationTick() {
         aniTick++;
-        if(aniTick >= aniSpeed){
+        if (aniTick >= aniSpeed) {
             aniTick = 0;
             aniIndex++;
-            if(aniIndex >= GetSpriteAmount(playerAction)){
+            if (aniIndex >= GetSpriteAmount(playerAction)) {
                 aniIndex = 0;
                 attacking = false;
             }
@@ -56,15 +57,15 @@ public class Player extends Entity{
 
         int startAni = playerAction;
 
-        if(moving)
+        if (moving)
             playerAction = RUNNING;
         else
             playerAction = IDLE;
 
-        if(attacking)
+        if (attacking)
             playerAction = ATTACK_1;
 
-        if(startAni != playerAction)
+        if (startAni != playerAction)
             resetAnitick();
 
 
@@ -78,22 +79,33 @@ public class Player extends Entity{
     private void updatePos() {
 
         moving = false;
+        if(!left && !right && !up && !down)
+            return;
 
-        if(left && !right){
-            x -= playerSpeed;
-            moving = true;
-        } else if (right && ! left) {
-            x += playerSpeed;
+        float xSpeed = 0, ySpeed = 0;
+
+        if (left && !right)
+            xSpeed -= playerSpeed;
+         else if (right && !left)
+            xSpeed += playerSpeed;
+
+        if (up && !down)
+            ySpeed -= playerSpeed;
+        else if (down && !up)
+            ySpeed += playerSpeed;
+
+//        if(CanMoveHere(x + xSpeed, y + ySpeed, width, height, lvlData)){
+//            this.x += xSpeed;
+//            this.y += ySpeed;
+//            moving = true;
+//        }
+
+        if(CanMoveHere(hitBox.x + xSpeed, hitBox.y + ySpeed, hitBox.width, hitBox.height, lvlData)){
+            hitBox.x += xSpeed;
+            hitBox.y += ySpeed;
             moving = true;
         }
 
-        if(up && !down){
-            y -= playerSpeed;
-            moving = true;
-        } else if (down && !up) {
-            y += playerSpeed;
-            moving = true;
-        }
     }
 
 
@@ -101,11 +113,15 @@ public class Player extends Entity{
         BufferedImage img = LoadSave.GetPlayerAtlas(LoadSave.PLAYER_ATLAS);
 
         animations = new BufferedImage[9][6];
-        for (int j = 0; j < animations.length; j++){
+        for (int j = 0; j < animations.length; j++) {
             for (int i = 0; i < animations[j].length; i++) {
                 animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
             }
         }
+    }
+
+    public void loadLvlData(int[][] lvlData){
+        this.lvlData = lvlData;
     }
 
     public void resetDirBoolean() {
@@ -115,7 +131,7 @@ public class Player extends Entity{
         down = false;
     }
 
-    public void setAttacking(boolean attacking){
+    public void setAttacking(boolean attacking) {
         this.attacking = attacking;
 
     }
